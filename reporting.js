b7b0c5161358025b1d6cacc96f172054f64553a1
Aravind <aravindar37@gmail.com>
2023-01-13 13:36:04 +0530

    Update reporting.js

--- reporting.js
+++ reporting.js
@@ -1,10 +1,17 @@
 exports = async function(arg){
-  let querydate = new Date();
-  querydate.setDate(querydate.getDate() - 1);
-  console.log(querydate);
-  const pipeline = [{ $match: { createdAt: { $gt: querydate } } }, { $unwind: { path: '$ruleViolation', preserveNullAndEmptyArrays: false } }, { $group: { _id: '$cluster', ruleViolation: { $addToSet: '$ruleViolation' } }}];
+  let dateQuery = new Date();
+  dateQuery.setDate(dateQuery.getDate() - 1);
+  const pipeline = [
+    { $match: { createdAt: { $gt: dateQuery } } }, 
+    {
+     $group: {
+        _id: {
+            cluster: '$cluster', namespace: '$namespace'
+        },
+        violations: { $addToSet: { rule: '$rule', field: '$field' }}
+      }
+    }];
   var collection = await context.services.get("Cluster0").db("contReview").collection("reviewResult");
   var result = await collection.aggregate(pipeline);
-  console.log(EJSON.stringify(result));
   return result;
 };
